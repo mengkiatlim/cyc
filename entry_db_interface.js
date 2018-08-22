1b4bf2a9a28ebc04b7e64da06600d894d77d069a
Lim Meng Kiat-RMJV83 <MengKiat.Lim@motorolasolutions.com>
2018-08-22 17:13:34 +0800

    combine interface and main

--- entry_db_interface.js
+++ entry_db_interface.js
@@ -50,6 +50,16 @@ const bounty_amount_id = "bounty_amount";
 const user_button_id = "user_button";
 const approver_button_id = "approver_button";
 
+// Client ID and API key from the Developer Console
+var CLIENT_ID;
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS;
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES;
+
 // ID of spreadsheet acting as database
 var SPREADSHEET_ID;
 
@@ -121,9 +131,12 @@ function handle_approver_button(event) {
     }
 }
 
-function init_db(new_spreadsheet_id, key_id) {
+function init_db(client_id, discovery_docs, scopes, new_spreadsheet_id, key_id) {
     //load_macro(div_id);
 
+    CLIENT_ID = client_id;
+    DISCOVERY_DOCS = discovery_docs;
+    SCOPES = scopes;
     SPREADSHEET_ID = new_spreadsheet_id;
     KEY_ID = key_id;
 
@@ -177,3 +190,49 @@ function save_entry(entry_data) {
 }
 
 function delete_entry(key) { }
+
+
+var authorizeButton = document.getElementById('authorize_button');
+
+/**
+  *  Initializes the API client library and sets up sign-in state
+  *  listeners.
+  */
+function initClient() {
+    gapi.client.init({
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES
+    }).then(function () {
+        // Listen for sign-in state changes.
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+        // Handle the initial sign-in state.
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        authorizeButton.onclick = handleAuthClick;
+        signoutButton.onclick = handleSignoutClick;
+    });
+}
+
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+        authorizeButton.style.display = 'none';
+        console.log("User signed in");
+        // Do something useful
+        load_entry(KEY_ID);
+    } else {
+        console.log("User not signed in");
+        authorizeButton.style.display = 'block';
+    }
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick(event) {
+    gapi.auth2.getAuthInstance().signIn();
+}
